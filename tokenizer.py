1629250ec41fdc30601790738e154fafcaab822e
thatboytemi <144232256+thatboytemi@users.noreply.github.com>
2024-11-27 21:30:23 +0200

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,11 +1,13 @@
 import sentencepiece as spm
 
-input_file = 'training_corpus.txt'
-model_prefix = 'model_name'
+input_file = '/scratch/anxtem001/extracted_data.txt'
+model_prefix = 'test_model'
 vocab_size = 50000
 
 spm.SentencePieceTrainer.train(
     input=input_file,
     model_prefix=model_prefix,
-    vocab_size=vocab_size
+    vocab_size=vocab_size,
+    train_extremely_large_corpus=True, 
+    input_sentence_size=7000000 #dataset is too large so have to sample sentences to train tokenizer on. Can possibly increase this value
 )
